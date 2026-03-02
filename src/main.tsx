9bbf4a22ab04b4856fddc6b1d2d948471f8e8921
ampflaretech <ampflaretech@gmail.com>
2026-03-02 13:54:59 +0500

    Add alert for TypeScript message
    Add a TypeScript message alert to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+let message: string = "Hello, TypeScript!";
+alert(message);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
