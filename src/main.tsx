3d842a913a7a3e309a9ad5b593b3445cc185e6ab
ampflaretech <ampflaretech@gmail.com>
2026-03-02 14:12:46 +0500

    Change alert message to 'Hello World!'

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-let message: string = "Hello, TypeScript!";
-alert(message);
+alert('Hello World!');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
